8d50a8a3bf91b018c5dbb7ea24ef1268057b8523
chihona <chihona1124@gmail.com>
2018-11-10 17:13:09 +0900

    js以外を作成
    HERO画像のjsは未設定

--- js/index.js
+++ js/index.js
@@ -1,1 +1,25 @@
- alert(1);
+$(function(){
+	setTimeout("rect()");
+});
+
+function rect() {
+	$("#pict1, #pict2, #pict3").animate({
+		marginTop: "-=10px"
+	}, 1000).animate({
+		marginTop: "+=10px"
+	}, 1000);
+
+	setTimeout("rect()");
+}
+
+
+$(function(){
+	setTimeout("move_bg()");
+});
+
+function move_bg(){
+	$(".top-wrapper, .recruit").animate({backgroundSize: "2020px"
+},3000).animate({backgroundSize: "1920px"},3000);
+
+setTimeout("move_bg()");
+}
